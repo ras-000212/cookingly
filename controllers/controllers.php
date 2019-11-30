21dc35bef53a3668d01bea41c462539d7ee77ff8
Alexis <>
2019-12-01 00:36:51 +0100

    add connexionDB in function

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -1,24 +1,24 @@
 <?php
-/*connect into the DB*/
-$host = 'localhost';
-$db   = 'galiixy';
-$user = 'galiixy';
-$pass = 'Jobslpxi';
-$charset = 'utf8mb4';
+/*function that connect*/
+function authentification(){
+    /*connect into the DB*/
+    $host = 'localhost';
+    $db   = 'galiixy';
+    $user = 'galiixy';
+    $pass = 'Jobslpxi';
+    $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-/*function that connect*/
-function authentification(){
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
     include ("./model/users.php");
     $login=$_POST['login'];
     $password=$_POST['password'];
