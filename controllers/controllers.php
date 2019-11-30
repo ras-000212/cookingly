a67c8f47177a93c66675bce051ad95b643a37a3b
Alexis <>
2019-12-01 00:24:02 +0100

    add connexion into the db

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -1,5 +1,22 @@
 <?php
+/*connect into the DB*/
+$host = 'localhost';
+$db   = 'galiixy';
+$user = 'galiixy';
+$pass = 'Jobslpxi';
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 /*function that connect*/
 function authentification(){
     include ("./model/users.php");
