e176fa766ddcb638a6d7cdb657fee55d28405f8c
Noelle15 <56430898+Noelle15@users.noreply.github.com>
2019-12-01 18:20:21 +0100

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -14,19 +14,19 @@
 
     <h1>Welcome to your account page</h1>
 
-    <p><a href="index.php?controle=users&action=change_login">Change your login</a></p>
-    <p><a href="index.php?controle=users&action=change_password">Change your password</a></p>
+    <p><a href="index.php?controle=controllers&action=change_login">Change your login</a></p>
+    <p><a href="index.php?controle=controllers&action=change_password">Change your password</a></p>
     <button onclick="myFunction()">Delete your account</button>
 
     <p id="demo"></p>
     <br/>
-    <p><a href="index.php?controle=users&action=fridge">Go to your fridge</a></p>
+    <p><a href="index.php?controle=controllers&action=fridge">Go to your fridge</a></p>
     <script>
     function myFunction() {
         var txt;
             if (confirm("Do you confirm the suppression of your account?","Confirmation")) {
                 txt = "You pressed OK!";
-                window.location.replace("index.php?controle=users&action=authentification");
+                window.location.replace("index.php?controle=controllers&action=authentification");
             } else {
                 txt = "You pressed Cancel!";
             }
