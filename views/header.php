bbea14b392ed6b39da1eeab1992ad9a71562f8a0
Noelle15 <56430898+Noelle15@users.noreply.github.com>
2019-12-01 17:41:13 +0100

    Update header.php

--- views/header.php
+++ views/header.php
@@ -11,12 +11,11 @@
 <body>
 
    	<header>
-		<h1>Bienvenue sur Cookingly</h1>
+		<h1>Welcome to Cookingly</h1>
 	</header>
 	<nav>
 		<ul>
 		    <li><a href="index.php?controle=controllers&action=fridge">Home</a></li>
-		    <li><a href="index.php?controle=controllers&action=fridge">Fridge</a></li>
 		    <li><a href="index.php?controle=controllers&action=profile">Profile</a></li>
 		    <li><a href="index.php?controle=controllers&action=signOut">Sign out</a></li>
 		</ul> 
