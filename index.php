242e8da2aefa644e124c48d60a88a487129db2f9
Noelle15 <56430898+Noelle15@users.noreply.github.com>
2019-12-02 22:58:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,24 @@
 <?php
-session_start();
+	session_start();
+	
+	if(isset($_SESSION['error'])){
+	    echo($_SESSION['error']);
+	}
+	
+	if (isset($_GET['controle']) and isset($_GET['action'])){
+	    $controle = $_GET['controle'];
+	    $action=$_GET['action'];
+	}
+	else{
+	    $controle ="controllers";
+	    $action="authentification";}
+	
+	require ('./controllers/' . $controle . '.php');
+	$action();
+	
+	function route($controle,$action){
+	    require ('./controlers/' . $controle .'php');
+	    $action();
+	}
 
-//if(isset($_SESSION['error'])){
-//    echo($_SESSION['error']);
-//}
 
-
-
-if (isset($_GET['controle']) and isset($_GET['action'])){
-    $controle = $_GET['controle'];
-    $action=$_GET['action'];
-}
-else{
-    $controle ="controllers";
-    $action="authentification";}
-
-require ('./controllers/' . $controle . '.php');
-$action();
-
-function route($controle,$action){
-    require ('./controlers/' . $controle .'php');
-    $action();
-}
-
-if(isset($_SESSION['error'])){
-		foreach($_SESSION['error'] as $type => $message):
-			echo($_SESSION['error']);
-		endforeach;
-		unset($_SESSION['error']);
-		}
