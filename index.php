909c69c837898dcb15689e791cb04040f676edbe
Noelle15 <56430898+Noelle15@users.noreply.github.com>
2019-12-02 23:17:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 	session_start();
 	
-	if(isset($_SESSION['error'])){
-	    echo($_SESSION['error']);
-	    unset($_SESSION['error']);
-	}
-	
 	if (isset($_GET['controle']) and isset($_GET['action'])){
 	    $controle = $_GET['controle'];
 	    $action=$_GET['action'];
 	}
 	else{
 	    $controle ="controllers";
-	    $action="authentification";}
+	    $action="authentification";
+	}
 	
 	require ('./controllers/' . $controle . '.php');
 	$action();
 	
+	if(isset($_SESSION['error'])){
+	      echo($_SESSION['error']);
+	      unset($_SESSION['error']);
+	}
+
 	function route($controle,$action){
 	    require ('./controlers/' . $controle .'php');
 	    $action();
