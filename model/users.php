11309a6fac37fa576ee8aed2f7042c8748802259
Galiixy <50149050+Galiixy@users.noreply.github.com>
2019-12-02 18:30:24 +0100

    Update users.php

--- model/users.php
+++ model/users.php
@@ -109,16 +109,16 @@ function remove_food_db($login,$food_name,$quantity){
     $res=$pdo->query($sql);
     
      if($row = $res->fetch()){
-         /*$row = $res->fetch();
+         /*$row = $res->fetch(); */
          $old_quantity = $row['quantity'];
          
          if($old_quantity>$quantity){ 
-         }*/
+         }
               $pdo->query("UPDATE Food set quantity=quantity-'$quantity' where Id_User='$IdUser' and Id_Food='$IdFood'");
-         /*
+        
          else{
              $pdo->query("DELETE From Food where Id_User='$IdUser' and Id_Food='$IdFood'");
-         }*/
+         }
      }
     }
 
