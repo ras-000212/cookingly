896bc1940b2fe0b563996f705e7be6897f8ef6ff
Galiixy <50149050+Galiixy@users.noreply.github.com>
2019-12-02 18:20:56 +0100

    Update users.php

--- model/users.php
+++ model/users.php
@@ -108,6 +108,8 @@ function remove_food_db($login,$food_name,$quantity){
     $sql ="Select quantity from Food where Id_Food='$IdFood' and Id_User='$IdUser'";
     $res=$pdo->query($sql);
      if($row = $res->fetch()){
+         $row = $res->fetch();
+         
          if($row['quantity']>$quantity){
               $pdo->query("UPDATE Food set quantity=quantity-'$quantity' where Id_User='$IdUser' and Id_Food='$IdFood'");
          }
